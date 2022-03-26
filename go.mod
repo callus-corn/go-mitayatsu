d36291b81bfe8cb360e58c063bf3f1f6ea857b54
callus-corn <mtfm_ymzk@yahoo.co.jp>
2022-03-26 18:22:48 +0900

    Update contentsdata flexble

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/callus-corn/go-mitayatsu
+module github.com/callus-corn/mitayatsu
 
 go 1.17
--- main.go
+++ main.go
@@ -1,51 +1,48 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
+	"os"
 	"strings"
 	"text/template"
 )
 
 type Contents struct {
-	Title     string
-	Star      string
-	Recommend string
-	Comment   string
-	Time      string
-	Media     string
+	Title string `json:"title"`
+	Worth string `json:"worth"`
+}
+
+type Review struct {
+	Title string
+	Text  string
 }
 
 func main() {
-	handleAdmin()
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/", contents)
+	http.HandleFunc("/", serveHTTP)
+	http.HandleFunc("/static/", serveStatic)
+	http.HandleFunc("/images/", serveImage)
 	http.HandleFunc("/api/upload", upload)
+	http.HandleFunc("/api/auth", auth)
 	http.ListenAndServe(":80", nil)
 }
 
-func contents(w http.ResponseWriter, req *http.Request) {
+func serveHTTP(w http.ResponseWriter, req *http.Request) {
 	path := req.URL.Path
 
 	if path == "/" {
-		render_index(w, req)
+		serveIndex(w, req)
 	} else {
-		render_contents(w, req)
+		serveContents(w, req)
 	}
 }
 
-func render_index(w http.ResponseWriter, req *http.Request) {
-	contentsTypes := []string{
-		"時間作ってでもみた方がいい",
-		"時間があるなら見た方がいい",
-		"暇なら見た方がいい",
-		"誰にでもオススメ",
-		"大体の人にオススメ",
-		"コアな人にオススメ",
-	}
-
-	contentsList := map[string][]Contents{}
-	for _, v := range contentsTypes {
-		contentsList[v] = getContentsList(v)
+func serveIndex(w http.ResponseWriter, req *http.Request) {
+	contentsList := getContentsList()
+	indexContents := map[string][]Contents{}
+	for _, v := range contentsList {
+		indexContents[v.Worth] = append(indexContents[v.Worth], v)
 	}
 
 	index, err := template.ParseFiles("templates/index.tmpl")
@@ -53,43 +50,79 @@ func render_index(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, err.Error(), 404)
 		return
 	}
-	if err = index.Execute(w, contentsList); err != nil {
+	if err = index.Execute(w, indexContents); err != nil {
 		http.Error(w, err.Error(), 404)
 	}
 }
 
-func render_contents(w http.ResponseWriter, req *http.Request) {
+func serveContents(w http.ResponseWriter, req *http.Request) {
 	title := strings.Replace(req.URL.Path, "/", "", 1)
-	contents := getContents(title)
+	contents, err := getContents(title)
+	if err != nil {
+		http.Error(w, err.Error(), 404)
+		return
+	}
+	review, err := getReview(title)
+	if err != nil {
+		http.Error(w, err.Error(), 404)
+		return
+	}
+
+	contentsData := map[string]string{
+		"Title":  title,
+		"Worth":  contents.Worth,
+		"Review": review.Text,
+	}
+
 	page, err := template.ParseFiles("templates/contents.tmpl")
 	if err != nil {
 		http.Error(w, err.Error(), 404)
 		return
 	}
-	if err = page.Execute(w, contents); err != nil {
+	if err = page.Execute(w, contentsData); err != nil {
 		http.Error(w, err.Error(), 404)
 	}
 }
 
-func getContentsList(contentsType string) []Contents {
-	c1 := Contents{
-		Title:     "プリパラ",
-		Star:      "時間作ってでもみた方がいい",
-		Recommend: "大体の人にオススメ",
-		Comment:   "みろ",
-		Time:      "30分×38話",
-		Media:     "dアニメストア",
+func getContentsList() []Contents {
+	result := []Contents{}
+	file, err := os.ReadFile("contentsdata/contents.json")
+	if err != nil {
+		return result
 	}
-	return []Contents{c1, c1, c1, c1, c1, c1, c1}
+	err = json.Unmarshal(file, &result)
+	if err != nil {
+		return result
+	}
+	return result
 }
 
-func getContents(title string) Contents {
-	return Contents{
-		Title:     "プリパラ",
-		Star:      "時間作ってでもみた方がいい",
-		Recommend: "大体の人にオススメ",
-		Comment:   "みろ",
-		Time:      "30分×38話",
-		Media:     "dアニメストア",
+func getContents(title string) (Contents, error) {
+	contentsList := getContentsList()
+	for _, v := range contentsList {
+		if v.Title == title {
+			return v, nil
+		}
 	}
+	return Contents{}, errors.New("404 Not Found")
 }
+
+func getReview(title string) (Review, error) {
+	text, err := os.ReadFile("contentsdata/review/" + title)
+	if err != nil {
+		return Review{}, err
+	}
+	return Review{Title: title, Text: string(text)}, nil
+}
+
+func serveStatic(w http.ResponseWriter, req *http.Request) {
+	http.StripPrefix("/static/", http.FileServer(http.Dir("static"))).ServeHTTP(w, req)
+}
+
+func serveImage(w http.ResponseWriter, req *http.Request) {
+	http.StripPrefix("/images/", http.FileServer(http.Dir("contentsdata/images"))).ServeHTTP(w, req)
+}
+
+func upload(w http.ResponseWriter, req *http.Request) {}
+
+func auth(w http.ResponseWriter, req *http.Request) {}
